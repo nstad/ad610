eda5783526e9590bcd5a0f38ced6918caf4f1c12
nstad <55989051+nstad@users.noreply.github.com>
2023-05-31 17:36:27 +0200

    Add files via upload

--- ad610app.ps1
+++ ad610app.ps1
@@ -7,10 +7,10 @@ while ((Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain -eq $false)
 sleep 120
 $passwordAD = ConvertTo-SecureString "AD661!Pa55w.rd" -AsPlainText -Force
 $adcred = New-Object System.Management.Automation.PSCredential ("saplab\ad-adm", $passwordAD )
-
 #DomainJoin
 Add-Computer -DomainName saplab.local -Credential $adcred
 
-#Restart Server
-Restart-Computer -Delay 120 -Force
 }
+
+#Restart Server
+Restart-Computer -Delay 20 -Force
